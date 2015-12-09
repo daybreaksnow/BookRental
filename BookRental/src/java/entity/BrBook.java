a88c2fe9f00b87b50f041ae2d3d909bc64827e7f
daybreaksnow <daybreaksnow@gmail.com>
2015-12-10 00:40:15 +0900

    refs #8 書籍の保存が行えるように

--- BookRental/src/java/entity/BrBook.java
+++ BookRental/src/java/entity/BrBook.java
@@ -1,11 +1,13 @@
 package entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  * 書籍クラス
@@ -13,9 +15,12 @@ import javax.validation.constraints.NotNull;
  * @since 2015/12/09
  */
 @Entity
+@Table(name = "br_book")
 public class BrBook implements Serializable{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "BR_BOOK_ID_SEQ_GEN")
+    @SequenceGenerator(name="BR_BOOK_ID_SEQ_GEN",sequenceName="BR_BOOK_ID_SEQ",allocationSize = 1)
+    @Column(name="book_id")
     private Long bookId;
     private String title;
     private String author;
@@ -27,5 +32,29 @@ public class BrBook implements Serializable{
         this.title = title;
         this.author = author;
     }
-    
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 }
