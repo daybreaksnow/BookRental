69789ddb35072d528d1dc68718c2bf26f99f54c5
Toshiyuki Imaizumi <daybreaksnow@gmail.com>
2015-12-10 13:39:05 +0900

    fixes #8,#11 書籍登録、一覧画面に出版社、出版日、総数を追加
    保存時に以下の例外が出る状態
    No validator could be found for type: java.util.Date

--- BookRental/src/java/beans/RegisterBookBean.java
+++ BookRental/src/java/beans/RegisterBookBean.java
@@ -2,11 +2,14 @@ package beans;
 
 import dao.BookDao;
 import entity.BrBook;
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * 書籍登録画面のバッキングビーン
@@ -16,13 +19,23 @@ import javax.validation.constraints.Size;
 @Named
 @RequestScoped
 public class RegisterBookBean {
-    @NotNull
+    @NotEmpty
     @Size(max=250)
     private String title;
     
-    @NotNull
+    @NotEmpty
     @Size(max=250)
     private String author;
+
+    @NotEmpty
+    private String publisher;
+    
+    @NotEmpty
+    private Date publicationDate;
+    
+    @NotEmpty
+    @Min(1)
+    private Integer totalNum;
     
     @EJB
     private BookDao dao;
@@ -42,14 +55,44 @@ public class RegisterBookBean {
     public void setAuthor(String author) {
         this.author = author;
     }
-    
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(Integer totalNum) {
+        this.totalNum = totalNum;
+    }
+
     public String next(){
         create();
         return "index?faces-redirect=true";
     }
     
     public void create(){
-        BrBook book = new BrBook(title,author);
+        BrBook book = new BrBook();
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublisher(publisher);
+        book.setPublicationDate(publicationDate);
+        book.setTotalNum(totalNum);
+        book.setReservedNum(0);
         try{
             dao.create(book);
         }
--- BookRental/src/java/entity/BrBook.java
+++ BookRental/src/java/entity/BrBook.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * 書籍クラス
@@ -22,15 +25,35 @@ public class BrBook implements Serializable{
     @SequenceGenerator(name="BR_BOOK_ID_SEQ_GEN",sequenceName="BR_BOOK_ID_SEQ",allocationSize = 1)
     @Column(name="book_id")
     private Long bookId;
+    
     private String title;
+    
     private String author;
+    
+    private String publisher;
+    
+    @Column(name="publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    
+    @Column(name="total_num")
+    private Integer totalNum;
+    
+    @Column(name="reserved_num")
+    private Integer reservedNum;
+    
+    @Column(name="create_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    
+    @Column(name="update_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+
+    private Long version;
+    
 
     public BrBook() {
-    }
-
-    public BrBook( String title, String author) {
-        this.title = title;
-        this.author = author;
     }
 
     public Long getBookId() {
@@ -57,4 +80,60 @@ public class BrBook implements Serializable{
         this.author = author;
     }
 
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(Integer totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    public Integer getReservedNum() {
+        return reservedNum;
+    }
+
+    public void setReservedNum(Integer reservedNum) {
+        this.reservedNum = reservedNum;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+    
 }
